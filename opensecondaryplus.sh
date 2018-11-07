2c784c3c671230494f4f6e63cdbebb3c18075edf
Devan Bicher <thedevanbicher@gmail.com>
2018-11-07 16:39:10 -0500

    Updated the opensecondaryplus script to use the path of the secondary structure folder

--- opensecondaryplus.sh
+++ opensecondaryplus.sh
@@ -4,4 +4,4 @@ pdb=$(echo "$1" | cut -d '-' -f1)
 
 #echo "$pdb"
 
-/home/devan/Installed/pymol/pymol $1 /home/devan/Research/proteins/$pdb+h.pdb /home/devan/Research/RNAs/$pdb-rna+h.pdb
+/home/devan/Installed/pymol/pymol /home/devan/Research/secondary_structures/$1 /home/devan/Research/proteins/$pdb+h.pdb /home/devan/Research/RNAs/$pdb-rna+h.pdb
